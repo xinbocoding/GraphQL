da0f25d6f584562f0e2b441de9908cea9aad8500
xinbo wang <xinbo.wang.dt@gmail.com>
2020-10-15 14:30:40 -0700

    connect graphlql to mongodb

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const express = require('express');
-const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema/schema');
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}));
+//connect to mongodb
+mongoose.connect(
+  "mongodb+srv://xinbo:12345@cluster0.tc4sb.mongodb.net/graphqlDemo?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to database");
+});
 
-app.listen(4000, ()=> {
-  console.log('the server is listening 4000 port');
-})
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+app.listen(4000, () => {
+  console.log("the server is listening 4000 port");
+});
